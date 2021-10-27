c027160ac78798113d0cfa428ddcb3bb39767854
yota konno <yota.rock.guitar@gmail.com>
2021-10-28 00:03:38 +0900

    secrets以外の接続識別子追加
    db接続確認

--- src/main/java/com/inhouse/dao/EventDao.java
+++ src/main/java/com/inhouse/dao/EventDao.java
@@ -22,7 +22,7 @@ public class EventDao {
     /**
      * イベントリストを取得するSQL
      */
-    private static final String SQL_SELETE_EVENT_LIST = "SELECT";
+    private static final String SQL_SELETE_EVENT_LIST = "SELECT community_id, event_id, title, start, end, location, description, thumbnail_img FROM event WHERE delete_flag = 0 and community_id = ?";
 
     public List<Event> getEventList(Long communityId) throws SQLException {
         List<Event> result = new ArrayList<Event>();
@@ -30,21 +30,24 @@ public class EventDao {
             pst.setLong (1,communityId);
             ConsoleLogger.debug(" RUN SQL : " + SQL_SELETE_EVENT_LIST + ", param : " + communityId.toString());
             try (ResultSet rs = pst.executeQuery()){
-                rs.next();
-                result.add(
-                    Event.builder()
-                        .communityId(rs.getLong("event_id"))
-                        .eventId(rs.getLong("event_id"))
-                        .title(rs.getString("title"))
-                        .start(rs.getTimestamp("start").toString())
-                        .end(rs.getTimestamp("end").toString())
-                        .locaiton(rs.getString("location"))
-                        .description(rs.getString("description"))
-                        .thumbnailImg(rs.getString("thumbnail_img"))
-                    .build()
-                );
+                while(rs.next()){
+                    result.add(
+                        Event.builder()
+                            .communityId(rs.getLong("community_id"))
+                            .eventId(rs.getLong("event_id"))
+                            .title(rs.getString("title"))
+                            .start(rs.getTimestamp("start").toString())
+                            .end(rs.getTimestamp("end").toString())
+                            .locaiton(rs.getString("location"))
+                            .description(rs.getString("description"))
+                            .thumbnailImg(rs.getString("thumbnail_img"))
+                        .build()
+                    );
+                }
             }
         }
         return result;
     }
 }
+
+
